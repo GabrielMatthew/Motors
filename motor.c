6612f45ed6d1b6a07472e8e211b3f238fe67697a
GabrielMatthew <60351385+GabrielMatthew@users.noreply.github.com>
2023-10-13 13:47:12 +0800

    Add files via upload

--- motor.c
+++ motor.c
@@ -68,25 +68,25 @@ else
 {
   if (myData.store_Humid >= param_Humid_Max && myData.store_Temp >= param_Temp_Max && manual_blinds = false)
   {
-    digitalWrite(IN1_PIN_WINDOW, HIGH);
-    digitalWrite(IN2_PIN_WINDOW, LOW);
-    analogWrite(ENA_PIN_WINDOW, 50);
+    digitalWrite(IN1_PIN_BLINDS, HIGH);
+    digitalWrite(IN2_PIN_BLINDS, LOW);
+    analogWrite(ENA_PIN_BLINDS, 50);
     delay(10000);
 
-    digitalWrite(IN1_PIN_WINDOW, LOW);
-    digitalWrite(IN2_PIN_WINDOW, LOW);
+    digitalWrite(IN1_PIN_BLINDS, LOW);
+    digitalWrite(IN2_PIN_BLINDS, LOW);
     manual_blinds = true;
   }
 
   else if (myData.store_Humid <= param_Humid_Min && myData.store_Temp <= param_Temp_Min && manual_blinds = true)
   {
-    digitalWrite(IN1_PIN_WINDOW, HIGH);
-    digitalWrite(IN2_PIN_WINDOW, LOW);
-    analogWrite(ENA_PIN_WINDOW, 50);
+    digitalWrite(IN1_PIN_BLINDS, HIGH);
+    digitalWrite(IN2_PIN_BLINDS, LOW);
+    analogWrite(ENA_PIN_BLINDS, 50);
     delay(10000);
 
-    digitalWrite(IN1_PIN_WINDOW, LOW);
-    digitalWrite(IN2_PIN_WINDOW, LOW);
+    digitalWrite(IN1_PIN_BLINDS, LOW);
+    digitalWrite(IN2_PIN_BLINDS, LOW);
     manual_blinds = true;
   }
 }
@@ -108,5 +108,61 @@ else
     digitalWrite(RELAY_PIN, HIGH); // turn on pump 15 seconds
     delay(15000);
     digitalWrite(RELAY_PIN, LOW);
+  }
+}
+
+//sets windows control to manual
+if (ctrl_Windows_manual = true)
+{
+  if (ctrl_Windows_onOff = true && manual_windows = false)
+  {
+    digitalWrite(IN1_PIN_WINDOW, HIGH);
+    digitalWrite(IN2_PIN_WINDOW, LOW);
+    analogWrite(ENA_PIN_WINDOW, 50);
+    delay(10000);
+
+    digitalWrite(IN1_PIN_WINDOW, LOW);
+    digitalWrite(IN2_PIN_WINDOW, LOW);
+    manual_blinds = true;
+  }
+
+  else 
+  {
+    digitalWrite(IN1_PIN_WINDOW, LOW);
+    digitalWrite(IN2_PIN_WINDOW, HIGH);
+    analogWrite(ENA_PIN_WINDOW, 50);
+    delay(10000);
+
+    digitalWrite(IN1_PIN_WINDOW, LOW);
+    digitalWrite(IN2_PIN_WINDOW, LOW);
+    manual_blinds = false;
+  }
+}
+
+//sets window control to automatic
+else
+{
+  if (myData.store_Wind >= param_Wind_Max && manual_windows = false)
+  {
+    digitalWrite(IN1_PIN_WINDOW, HIGH);
+    digitalWrite(IN2_PIN_WINDOW, LOW);
+    analogWrite(ENA_PIN_WINDOW, 50);
+    delay(10000);
+
+    digitalWrite(IN1_PIN_WINDOW, LOW);
+    digitalWrite(IN2_PIN_WINDOW, LOW);
+    manual_blinds = true;
+  }
+
+  else if (myData.store_Wind <= param_Wind_Min && manual_windows = true)
+  {
+    digitalWrite(IN1_PIN_WINDOW, HIGH);
+    digitalWrite(IN2_PIN_WINDOW, LOW);
+    analogWrite(ENA_PIN_WINDOW, 50);
+    delay(10000);
+
+    digitalWrite(IN1_PIN_WINDOW, LOW);
+    digitalWrite(IN2_PIN_WINDOW, LOW);
+    manual_blinds = true;
   }
 }
